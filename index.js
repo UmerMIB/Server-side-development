78dc5f5a0c298bb4e28ed300860a629e765f378e
Umer MiB <mohammadumer704@gmail.com>
2019-12-11 19:02:40 +0500

    callbacks and error handling

---
+++ index.js
@@ -0,0 +1,17 @@
+var rect = require("./rectangle");
+
+function solveRect(l, b) {
+    console.log(` solving rectangle for length ${l} and breath ${b} `);
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log(`Error`, err.message);
+        } else {
+            console.log(` Area is ${ rectangle.area() } and rectangle is ${ rectangle.perimeter() } `)
+        }
+    })
+    console.log(`this statement is after the callback`);
+}
+
+solveRect(2, 1);
+solveRect(2, 2);
+solveRect(2, 3);
