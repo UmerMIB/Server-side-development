670dd05a9ed9ea5d945f2d8e6d2437f091ec0ffb
Umer MiB <mohammadumer704@gmail.com>
2019-12-18 17:28:44 +0500

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,17 +1,49 @@
 const mongoose = require('mongoose'),
     dishes = require('./models/dishes'),
     url = 'mongodb://localhost:27017/conFusion';
+// mongoose.connect(url)
+//     .then((db) => {
+//         console.log(` Server correctly connected `);
+//         var newDish = dishes({
+//             name: 'umer1 ',
+//             description: 'umer testing here1'
+//         });
+//         newDish.save()
+//             .then((dish) => {
+//                 console.log('dishes are', dish);
+//                 return dishes.find({})
+//             })
+//             .then((dish) => {
+//                 console.log('all dishes are', dish);
+//                 return dishes.remove()
+//             })
+//             .then(() => {
+//                 mongoose.connection.close()
+//             })
+//             .catch((err) => {
+//                 console.log(' Error occured', err)
+//             })
+//     })
+//     .catch((err) => {
+//         console.log('server cannot  connect to the server err is ', err)
+//     })
+
+
+
+
+
+
+
 mongoose.connect(url)
     .then((db) => {
         console.log(` Server correctly connected `);
-        var newDish = dishes({
-            name: 'umer1 ',
-            description: 'umer testing here1'
-        });
-        newDish.save()
+        dishes.create({
+                name: 'umer1 ',
+                description: 'umer testing here1'
+            })
             .then((dish) => {
                 console.log('dishes are', dish);
-                return dishes.find({})
+                return dishes.find({}).exec()
             })
             .then((dish) => {
                 console.log('all dishes are', dish);
