ccaf1fa9bd334e0d19bdcd9c19e0b0676abc0986
UmerMIB <mohammadumer704@gmail.com>
2020-03-10 08:11:50 -0700

    initial commit

--- callbacks and error handling/rectangle.js
+++ callbacks and error handling/rectangle.js
@@ -1,21 +1,22 @@
 module.exports = (x, y, callback) => {
-    if (x <= 0 || y <= 0) {
-        setTimeout(() => {
-            callback(new Error(` The values of length and breath should be greater than 0 `),
-                    null),
-                2000
-        });
-    } else {
-        setTimeout(() => {
-            callback(null, {
-                    perimeter: () => (2 * (x * y)),
-                    area: () => (x * y),
-                }),
-                2000
-        });
-    }
-}
-
+  if (x <= 0 || y <= 0) {
+    setTimeout(() => {
+      callback(
+        new Error(` The values of length and breath should be greater than 0 `),
+        null
+      ),
+        2000;
+    });
+  } else {
+    setTimeout(() => {
+      callback(null, {
+        perimeter: () => 2 * (x * y),
+        area: () => x * y
+      }),
+        2000;
+    });
+  }
+};
 
 // export default (x, y, callback) => {         //ES6 module
 //     if (x <= 0 || y <= 0) {
